477cc3336d76b3826c4871c4a9a5825d4b47c92e
Salz <faisal_animator@yahoo.com>
2024-03-10 09:08:01 +0700

    feat: add text & change color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,10 +29,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.tealAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page - Counter'),
     );
   }
 }
@@ -112,6 +113,13 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text('Mochammad Faisal', textAlign: TextAlign.center),
+              ))
+            ]),
           ],
         ),
       ),
